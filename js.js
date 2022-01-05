317504b2e57f91f37c322f3c7725386b88f81096
abderrahim-elham <abderrahim.elhamdaoui2@gmail.com>
2022-01-05 17:29:54 +0000

    clock

--- js.js
+++ js.js
@@ -1,16 +1,17 @@
      var text=document.querySelector('.clock');
-     var mori=document.querySelectorAll('.mori');
+     var aiguille=document.querySelectorAll('.aiguille');
      var clock_min=document.querySelector('.clock-min');
      var mus=document.querySelector('#mus');
+
      function update()
      {
      var time=new Date();
-     var h=time.getHours();
-     var m=time.getMinutes();
-     var s=time.getSeconds();
-     mori[0].style.transform=`rotate(`+s*6+`deg)`;
-     mori[1].style.transform=`rotate(`+m*6+`deg)`;
-     mori[2].style.transform=`rotate(`+h*30+`deg)`;
+     var h=time.getUTCHours();
+     var m=time.getUTCMinutes();
+     var s=time.getUTCSeconds();
+     aiguille[0].style.transform=`rotate(`+s*6+`deg)`;
+     aiguille[1].style.transform=`rotate(`+m*6+`deg)`;
+     aiguille[2].style.transform=`rotate(`+h*30+`deg)`;
      
      clock_min.innerHTML=h+':'+m+':'+s+'<br><span style="color:cyan;">'+time.getDate()+'/'+(time.getMonth()+1)+'/'+time.getFullYear()+'</span>';
      }
